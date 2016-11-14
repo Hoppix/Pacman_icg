ef200f3641820462803f555bba76d28999809c7c
Hoppix <k.hopfmann@hotmail.de>
2016-11-14 17:51:33 +0100

    new branch: animation funktioniert nicht korrekt.

--- pacman script/pacman.js
+++ pacman script/pacman.js
@@ -1,5 +1,17 @@
 var gl;
-
+//Variablen für Position und Orientierung(Aufgabe 2) in Rad.
+var gLocX = 0.0;
+var gLocY = 0.0;
+var gAlign = degreesToRadians(0.0);
+//Polygone definieren
+var gintNumberOfVertices = 80;
+// Specify position and color of the vertices
+var gf32aVertices;
+var gf32aColors;
+//hilfsarray
+var gobaHelpMe;
+//renderprogramm
+var program;
 
 window.onload = function init()
 {
@@ -8,19 +20,9 @@ window.onload = function init()
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) { alert("WebGL isn't available"); }
 
-	// Specify position and color of the vertices
-	var gf32aVertices;
-	var gf32aColors;
-	var gobaHelpMe;
-	var gintNumberOfVertices = 80;
-	//Variablen für Position und Orientierung(Aufgabe 2) in Rad.
-	var gLocX = 0.0;
-	var gLocY = 0.0;
-	var gAlign = degreesToRadians(0.0);
-
 	window.addEventListener("keypress", eventHandling);
 
-	gobaHelpMe = drawPacman(3, gintNumberOfVertices, 45);
+	gobaHelpMe = drawPacman(0.75, gintNumberOfVertices, 45);
 	gf32aVertices = gobaHelpMe[0];
 	gf32aColors = gobaHelpMe[1];
 
@@ -29,28 +31,35 @@ window.onload = function init()
 	gl.clearColor(0.0,0.0,0.0,1.0);
 
 	// Init shader program and bind it
-	var program = initShaders(gl, "vertex-shader", "fragment-shader");
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
 	gl.useProgram(program);
-	setTransformMatrix(gAlign, gLocX, gLocY, program);
 
 	// Load positions into the GPU and associate shader variables
 	//VBO
-	setVBO(gf32aVertices, gf32aColors, program);
-  requestAnimFrame(render(gobaHelpMe[2]));
+	setVBO();
+	setTransformMatrix();
+	render();
+  requestAnimFrame(frameRender());
   //auf Index 2 von HelpMe steht NumberofVertices+2 -aussparung
-
 };
 
-function render(pintNumberOfVertices)
+function frameRender()
+{
+	gAlign = gAlign+degreesToRadians(10);
+	setTransformMatrix();
+	render();
+	requestAnimFrame(frameRender());
+}
+
+function render()
 {
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, pintNumberOfVertices+1);
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, gobaHelpMe[2]+1);
 }
 
 //berechnet die für das Polygon benötigten Vertices
 function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 {
-
 	gf32aVertices = new Float32Array((pintNumberOfVertices+2)*2);
 	gf32aColors = new Float32Array((pintNumberOfVertices+4)*4);
 
@@ -69,10 +78,8 @@ function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 	var lintShiftI = Math.PI/2 + Math.PI*(pintMouthAngle/(360));
 	var lintEnd = Math.round(pintNumberOfVertices - (pintMouthAngle/360) * pintNumberOfVertices)+1;
 
-
 	for(var i = 0; i<=lintEnd; i++ )
 	{
-
 		gf32aVertices[2*i+2] = (Math.sin((2*Math.PI/pintNumberOfVertices)*i+lintShiftI)*triangleLength);
 		gf32aVertices[2*i+3] = (Math.cos((2*Math.PI/pintNumberOfVertices)*i+lintShiftI)*triangleLength);
 
@@ -80,7 +87,6 @@ function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 		gf32aColors[4*i+1] = 1;
 		gf32aColors[4*i+2] = 0;
 		gf32aColors[4*i+3] = 1;
-
 	}
 
 	lobaHelpMe = [gf32aVertices, gf32aColors, lintEnd];
@@ -92,15 +98,15 @@ function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 
 }
 
-function setTransformMatrix(pAlign, pLocX, pLocY, pProgram)
+function setTransformMatrix()
 {
-	var transformMatrix = [Math.cos(pAlign), -(Math.sin(pAlign)), 0.0, pLocX,
-				                Math.sin(pAlign), Math.cos(pAlign)    , 0.0, pLocY,
+	var transformMatrix = [Math.cos(gAlign), -(Math.sin(gAlign)), 0.0, gLocX,
+				                Math.sin(gAlign), Math.cos(gAlign)    , 0.0, gLocY,
 								0.0                  , 0.0                        , 1.0,     0.0,
 								0.0                  , 0.0                        , 0.0,    1.0];
 
 
-	var matrixLoc =  gl.getUniformLocation(pProgram, "transformMatrix");
+	var matrixLoc =  gl.getUniformLocation(program, "transformMatrix");
 	gl.uniformMatrix4fv(matrixLoc, false, transformMatrix );
 	//Übergibt die Transformationsmatrix an den Vertex-Shader.
 }
@@ -110,13 +116,13 @@ function degreesToRadians(x)
 	return x * (Math.PI / 180);
 }
 
-function setVBO(pf32aVertices, pf32aColors, pProgram)
+function setVBO()
 {
 	var posVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
-	gl.bufferData(gl.ARRAY_BUFFER, pf32aVertices, gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, gf32aVertices, gl.STATIC_DRAW);
 
-	var vPosition = gl.getAttribLocation(pProgram, "vPosition");
+	var vPosition = gl.getAttribLocation(program, "vPosition");
 	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vPosition);
 
@@ -124,35 +130,45 @@ function setVBO(pf32aVertices, pf32aColors, pProgram)
 
 	var colorVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
-	gl.bufferData(gl.ARRAY_BUFFER, pf32aColors, gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, gf32aColors, gl.STATIC_DRAW);
 
-	var vColor = gl.getAttribLocation(pProgram, "vColor");
+	var vColor = gl.getAttribLocation(program, "vColor");
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vColor);
 }
 
 function eventHandling(e)
 {
-	//TODO functionen implementieren.
 	switch(e.keyCode)
 	{
 		case 38:
-					//foward
-					//move();
-					alert("foward");
+					moveFoward();
 					break;
 		case 37:
-					alert("left");
-					//Turn left
-					//turnLeft();
+					turnLeft();
 					break;
 		case 39:
-					alert("right");
-					//Turn right
-					//turnRight();
+					turnRight();
 					break;
 		default:
-					alert("Unzulässige Taste!");
+					alert(gAlign);
+					//alert(setTransformMatrix())
 					break;
 	}
+}
+
+function turnLeft()
+{
+	gAlign = gAlign + degreesToRadians(90);
+}
+
+function turnRight()
+{
+	gAlign = gAlign - degreesToRadians(90);
+}
+
+function moveFoward()
+{
+	gLocX = gLocX;
+	gLocY = gLocY;
 }
