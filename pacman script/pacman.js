0c4999d6d78cc3fd831a21f8f531cf6c84c86490
Hoppix <k.hopfmann@hotmail.de>
2016-11-14 19:53:13 +0100

    Zeilenkommentare hinzugefügt.

--- pacman script/pacman.js
+++ pacman script/pacman.js
@@ -1,9 +1,9 @@
 var gl;
-//Variablen für Position und Orientierung(Aufgabe 2) in Rad.
+//Variablen für Position und Orientierung.
 var gLocX = 0.0;
 var gLocY = 0.0;
 var gAlign = degreesToRadians(0.0);
-//Polygone definieren
+//Anzahl der Polygone definieren
 var gintNumberOfVertices = 80;
 // Specify position and color of the vertices
 var gf32aVertices;
@@ -20,8 +20,10 @@ window.onload = function init()
 	gl = WebGLUtils.setupWebGL(canvas);
 	if (!gl) { alert("WebGL isn't available"); }
 
+	//Event für Pfeiltasteninput
 	window.addEventListener("keypress", eventHandling);
 
+	//Zeichnen des Pacman auf dem ersten frame.
 	gobaHelpMe = drawPacman(0.75, gintNumberOfVertices, 45);
 	gf32aVertices = gobaHelpMe[0];
 	gf32aColors = gobaHelpMe[1];
@@ -34,13 +36,13 @@ window.onload = function init()
 	program = initShaders(gl, "vertex-shader", "fragment-shader");
 	gl.useProgram(program);
 
-	// Load positions into the GPU and associate shader variables
-	//VBO
+	//konfigurieren der Vertex-Buffer-Objects
 	setVBO();
 	setTransformMatrix();
+
+	//rendern des ersten frames und initialisierung des Frame-buffers.
 	render();
   requestAnimFrame(frameRender);
-  //auf Index 2 von HelpMe steht NumberofVertices+2 -aussparung
 };
 
 function frameRender()
@@ -52,11 +54,13 @@ function frameRender()
 
 function render()
 {
+	//render via TRIANGLE_FAN von Zentrum aus.
+	//gobaHelpMe[2]+1 = Vertices für die  Mundausparung
 	gl.clear(gl.COLOR_BUFFER_BIT);
 	gl.drawArrays(gl.TRIANGLE_FAN, 0, gobaHelpMe[2]+1);
 }
 
-//berechnet die für das Polygon benötigten Vertices
+
 function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 {
 	gf32aVertices = new Float32Array((pintNumberOfVertices+2)*2);
@@ -77,6 +81,10 @@ function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 	var lintShiftI = Math.PI/2 + Math.PI*(pintMouthAngle/(360));
 	var lintEnd = Math.round(pintNumberOfVertices - (pintMouthAngle/360) * pintNumberOfVertices)+1;
 
+
+	//berechnet wieviele Dreiecke beim erstellen des Kreises ausgelassen werden müssen um den Mund darzustellen.
+	//Der Mund soll bei Pi/2 dargestellt werden
+	//zentrum des Mundes bei Pi/2, darüber und darunter MouthAngle/2 winkel auslassen
 	for(var i = 0; i<=lintEnd; i++ )
 	{
 		gf32aVertices[2*i+2] = (Math.sin((2*Math.PI/pintNumberOfVertices)*i+lintShiftI)*triangleLength);
@@ -88,17 +96,14 @@ function drawPacman(radius, pintNumberOfVertices, pintMouthAngle)
 		gf32aColors[4*i+3] = 1;
 	}
 
+	//Return des localen hilfsarrays
 	lobaHelpMe = [gf32aVertices, gf32aColors, lintEnd];
 	return lobaHelpMe;
-
-//berechnet wieviele Dreiecke beim erstellen des Kreises ausgelassen werden müssen um den Mund dar zu stellen.
-//Der Mund soll bei Pi/2 dargestellt werden
-//zentrum des Mundes bei Pi/2, darüber und darunter MouthAngle/2 winkel auslassen
-
-}
+	}
 
 function setTransformMatrix()
 {
+	//Aufsetzen der Transformationsmatrix für Rotation und Translation.
 	var transformMatrix = [Math.cos(gAlign), -(Math.sin(gAlign)), 0.0, gLocX,
 				                Math.sin(gAlign), Math.cos(gAlign)    , 0.0, gLocY,
 								0.0                  , 0.0                        , 1.0,     0.0,
@@ -112,11 +117,14 @@ function setTransformMatrix()
 
 function degreesToRadians(x)
 {
+	//Einfache Hilfsfunktion zum Konvertieren von Grad zu Bogenmaß.
 	return x * (Math.PI / 180);
 }
 
 function setVBO()
 {
+	// Load positions into the GPU and associate shader variables
+
 	var posVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
 	gl.bufferData(gl.ARRAY_BUFFER, gf32aVertices, gl.STATIC_DRAW);
@@ -138,6 +146,8 @@ function setVBO()
 
 function eventHandling(e)
 {
+	//Auslesen des keycodes zum spezifizieren welche Taste gedrückt wurde.
+	//38: ArrowUp, 37: ArrowLeft, 39: ArrowRight
 	switch(e.keyCode)
 	{
 		case 38:
